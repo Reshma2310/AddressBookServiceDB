3af7d5911dfa854376035360f44038cb8297ef00
Reshma2310 <dreshma253@gmail.com>
2022-06-19 11:25:29 +0530

    [RESHMA] - Creating Address Book Table with given Columns

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -1,3 +1,7 @@
 UC1: Creating Address Book Service DB
 CREATE DATABASE AddressBook;
 use AddressBook;
+
+UC2: Create Address book table
+Create Table AddressBook (ID INT IDENTITY(1,1) PRIMARY KEY, FirstName VARCHAR(150), LastName VARCHAR(150), 
+Address VARCHAR(150), City VARCHAR(150), State VARCHAR(150), Zip INT, PhoneNumber INT, Email VARCHAR(150));
